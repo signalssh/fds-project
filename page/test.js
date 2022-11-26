f900def03b1b9145171ef7e11abd289826141440
shin <signalssh@naver.com>
2022-11-26 15:17:30 +0900

    first commit

--- page/test.js
+++ page/test.js
@@ -5,11 +5,13 @@
 function square(number){
     var value = 1;
     
-    console.log(this.value);
+    hgconsole.log(this.value);
     //console.log(this);
 
     return number*number;
 }
+
+
 
 square(2);
 //console.log(this === window);
